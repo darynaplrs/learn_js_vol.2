9dbe26599700d9f487b1fa4d6571ab84345044a5
Daryna <darinapolarus@gmail.com>
2024-01-20 21:31:15 +0100

    Adding 2 functions about resting dots

--- functions.js
+++ functions.js
@@ -1,3 +1,16 @@
 "use strict"
 
-alert("Finally!");
+function sumAll(...args){
+	let sum = 0;
+	for (let item of args){
+		sum += +item;
+	}
+	return sum;
+}
+
+function theRest (arg1, arg2, arg3, ...rest){
+	console.log(`This is the first argument: ${arg1}`);
+	console.log(`This is the second argument: ${arg2}`);
+	console.log(`This is the third argument: ${arg3}`);
+	console.log(`And this is the rest: ${rest.join(", ")}`);
+}
